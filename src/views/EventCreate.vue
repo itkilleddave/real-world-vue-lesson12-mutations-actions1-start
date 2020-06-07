c5c12073ea2c0b4931d0f348e6bd87f239effd55
Dave Longbottom <davelongbottom@gmail.com>
2020-06-07 10:44:44 +0930

    fixed EventCreate after adding form

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,15 +1,72 @@
 <template>
   <div>
     <h1>Create an Event, {{ user.name }}</h1>
-    <p>This event was created by {{ user.name }}</p>
-    <p>{{ getEventById(2) }}</p>
+    <form>
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <!-- <option v-for="cat in categories" :key="cat">{{ cat }}</option> -->
+      </select>
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input v-model="event.title" type="text" placeholder="Add an event title"/>
+      </div>
+      <div class="field">
+        <label>Description</label>
+        <input v-model="event.description" type="text" placeholder="Add a description"/>
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input v-model="event.location" type="text" placeholder="Add a location"/>
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date"/>
+      </div>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <!-- <option v-for="time in times" :key="time">{{ time }}</option> -->
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit"/>
+    </form>
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex'
+import Datepicker from 'vuejs-datepicker';
 
 export default {
+  data() {
+    return {
+      event: {
+        title: "Park Cleanup",
+        date: "Nov 12 2018",
+        time: "12:00",
+        location: "132 N Magnolia Street, Orlando, Florida",
+        description: "We're going to clean up this park.",
+        organizer: "Adam Jahr",
+        category: "nature",
+        attendees: [
+          // {
+          //   id: "ghi789",
+          //   name: "Beth Swanson"
+          // },
+          // {
+          //   id: "jkl101",
+          //   name: "Mary Gordon"
+          // }
+        ]
+      }
+    }
+  },
+  components: {
+    Datepicker
+  },
   computed: {
     ...mapGetters(['getEventById']),
     ...mapState(['user', 'categories'])
@@ -18,4 +75,7 @@ export default {
 </script>
 
 <style scoped>
+  .field {
+    margin-bottom: 24px;
+  }
 </style>
